187eae8393c19dfdd1a53f4c544ed2db1be1eb72
Justin Cheung <38864290+justinpakming@users.noreply.github.com>
2019-03-10 23:26:09 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+
+var landing = anime({
+  targets: 'div.landing',
+  translateY: h * 0.40,
+  scale: 0.5,
+  opacity: 0,
+  duration: h * 0.60,
+  delay: h * 0.15,
+  easing: 'linear',
+  autoplay: false
+});
+
+var title = anime({
+  targets: '.title',
+  opacity: 1,
+  duration: h * 0.25,
+  delay: h * 0.20,
+  easing: 'linear',
+  autoplay: false
+});
+
+window.addEventListener('scroll', () =>{
+  const scrolled = window.scrollY;
+  landing.seek(scrolled);
+  title.seek(scrolled);
+});
+
+$(document).ready(function() {
+  $('.particle').particleground({
+    dotColor: '#e56d7a',
+    lineColor: '#e56d7a',
+    density: 15000
+  });
+});
